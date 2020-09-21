41472f2cd8fb15306b0d37af0eb719f73d2246fd
Marcus Carey <marcus.carey@twotimessolutions.com>
2020-09-21 13:05:33 -0500

    * SqlServerCoursesRepository.cs: Add missing file from initial   commit
    * tea_submission.sln: Add readme

---
+++ student_api/Repositories/SqlServerCoursesRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using student_api.Models;
+
+namespace student_api.Repositories
+{
+    public class SqlServerCoursesRepository : ICoursesRepository
+    {
+        private readonly SchoolContext _context;
+
+        public SqlServerCoursesRepository(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public bool Exists(int courseId)
+        {
+            return _context.Courses.Any(c => c.CourseID == courseId);
+        }
+    }
+}
