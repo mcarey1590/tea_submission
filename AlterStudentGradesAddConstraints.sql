f2f9dfc3ab604d5880a40061a2a3ddddd646f71b
Marcus Carey <marcus.carey@twotimessolutions.com>
2020-09-21 13:09:22 -0500

    Add missing solution items

---
+++ AlterStudentGradesAddConstraints.sql
@@ -0,0 +1,33 @@
+﻿
+USE School
+GO
+
+ALTER TABLE StudentGrade ADD CONSTRAINT CHK_GRADE CHECK (Grade BETWEEN 0.0 AND 4.0)
+
+GO
+
+WITH cte AS (
+    SELECT 
+        EnrollmentID
+        ,CourseID
+        ,StudentID
+        ,Grade
+        ,ROW_NUMBER() OVER (
+            PARTITION BY 
+                CourseID
+                ,StudentID
+                --,Grade 
+            ORDER BY 
+                CourseID
+                ,StudentID
+                --,Grade
+        ) row_num
+     FROM 
+        StudentGrade
+)
+DELETE FROM cte
+WHERE row_num > 1;
+
+GO
+
+ALTER TABLE StudentGrade ADD CONSTRAINT UQ_COURSEID_STUDENTID UNIQUE NONCLUSTERED ( [CourseID], [StudentID] )
